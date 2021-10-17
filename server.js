ce0b5ca892009962da3f08b4a0a4d91a7d31ee00
Kevin <baquerochavarro@gmail.com>
2021-10-16 19:49:41 -0500

    integrar Auth0 para autenticacion en backend

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ import {
     conectarDB,
     getDB
 } from './db/db.js'
+import jwt from "express-jwt";
+import jwtAuthz from "express-jwt-authz";
+import jwksRsa from "jwks-rsa";
 import rutasProducto from "./views/productos/productoRutas.js";
 import rutasUsuario from "./views/usuarios/usuarioRutas.js";
 import rutasVenta from "./views/ventas/ventaRutas.js";
@@ -18,7 +21,25 @@ dotenv.config({
 });
 
 const app = Express()
-app.use(Express.json(), Cors(), rutasProducto, rutasUsuario, rutasVenta);
+app.use(Express.json());
+app.use(Cors());
+const checkJwt = jwt({
+    secret: jwksRsa.expressJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: `https://fanta-stick.us.auth0.com/.well-known/jwks.json`
+    }),
+
+    audience: 'https://api-autenticacion-fanta-store',
+    issuer: [`https://fanta-stick.us.auth0.com/`],
+    algorithms: ['RS256']
+});
+
+app.use(checkJwt)
+app.use(rutasProducto);
+app.use(rutasUsuario);
+app.use(rutasVenta);
 
 const main = () => {
     return app.listen(process.env.PORT, () => {
